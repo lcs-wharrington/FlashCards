8169c800ca279f8d42939571ecaa07bfa92b7ce2
William Harrington <wharrington@lcs.on.ca>
2021-12-07 14:13:41 -0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by William Robert Harrington on 2021-12-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
